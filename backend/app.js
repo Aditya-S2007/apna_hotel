297bf993cf1938112e83c290a9b3e3347a9bee70
Aditya Sharma <nomnomcat055@gmail.com>
2026-04-24 01:28:52 +0530

    joi api

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,8 @@ const path =require("path");
 const methodOverride=require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync= require("./utils/wrapAsync.js");
+const ExpressError= require("./utils/ExpressError.js");
+const { listingSchema } = require("./schema.js");
 
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
@@ -25,11 +27,23 @@ app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname,"/public")));
 
+const validateListing = (req, res, next) => {
+  let {error} = listingSchema.validate(req.body);
+  
+  if (error) {
+    let errMsg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(400, errMsg);
+  } else{
+    next();
+  }
+};
+
 // index route 1.
-app.get("/listings",async(req,res)=>{ // to print title data adn indi id in index ejs in loop
+app.get("/listings",wrapAsync( async(req,res)=>{ // to print title data adn indi id in index ejs in loop
     const allListings=await Listing.find({});
     res.render("listings/index.ejs",{allListings});
-});
+})
+);
 
 // new route 3.
 app.get("/listings/new",(req,res)=>{  // 1. index ejs new get req "/listins/new" in form botton main page
@@ -37,16 +51,18 @@ app.get("/listings/new",(req,res)=>{  // 1. index ejs new get req "/listins/new"
 });
 
 // show route 2.
-app.get("/listings/:id", async(req,res)=>{ // 1. req from index route 2. take req id and find 3. render and print all detail in show ejs
+app.get("/listings/:id",wrapAsync( async(req,res)=>{ // 1. req from index route 2. take req id and find 3. render and print all detail in show ejs
     let {id}= req.params;
     const listing = await Listing.findById(id);
     res.render("listings/show.ejs",{listing});
-});
+})
+);
 
 //create route 4.
-app.post("/listings", wrapAsync( async(req,res,next)=>{  // 1. all data will come in the body 2.exract and use mongodb single insert command
+app.post("/listings",validateListing, wrapAsync( async(req,res,next)=>{  // 1. all data will come in the body 2.exract and use mongodb single insert command
  // {title,description,img....location}=req.body old method
  //let listing=req.body.listing;
+ listingSchema.validate(req.body);
  const newlisting= new Listing(req.body.listing);  // tips: best way to reqire from body 'name "listing[title]"' in new.ejs
  await newlisting.save();
  res.redirect("/listings");
@@ -54,28 +70,35 @@ app.post("/listings", wrapAsync( async(req,res,next)=>{  // 1. all data will com
 );
 
 //edit route 5.
-app.get("/listings/:id/edit",async(req,res)=>{ //1. show ejs add link /id req 2. edit.ejs form to take data and in action=send put req 
+app.get("/listings/:id/edit", wrapAsync( async(req,res)=>{ //1. show ejs add link /id req 2. edit.ejs form to take data and in action=send put req 
     let {id}= req.params;
     const listing = await Listing.findById(id);
     res.render("listings/edit.ejs",{listing});
-});
+})
+);
 
 // update route 6.
-    app.put("/listings/:id", async(req,res)=>{ // 1. got all data frrom edit.ejs 
-    let {id}=req.params;
-    await Listing.findByIdAndUpdate(id,{...req.body.listing}); //2. find and update though put req 
+app.put(
+  "/listings/:id",validateListing,
+  wrapAsync(async (req, res) => {
+    // 1. got all data frrom edit.ejs
+   
+    let { id } = req.params;
+    await Listing.findByIdAndUpdate(id, { ...req.body.listing }); //2. find and update though put req
     res.redirect(`/listings/${id}`);
     // console.log(req.body.listing);
-});
+  }),
+);
     
 
 // delete route
-app.delete("/listings/:id",async(req,res)=>{ // 1. show.ejs add form action "/listings/:id" (for delete req)
+app.delete("/listings/:id",wrapAsync( async(req,res)=>{ // 1. show.ejs add form action "/listings/:id" (for delete req)
     let {id}=req.params;
     let deleteListing= await Listing.findByIdAndDelete(id); // 2. find and delete
     res.redirect("/listings");
     console.log(deleteListing);
-}); 
+})
+); 
 
 
 // app.get("/testListing", async(req,res)=>{
@@ -93,9 +116,13 @@ app.delete("/listings/:id",async(req,res)=>{ // 1. show.ejs add form action "/li
 app.get("/",(req,res)=>{
    res.send("I AM GROOT");
 });
-
+app.all(/(.*)/,(req,res,next)=>{
+    next(new ExpressError(505,"page not found..!"));
+});
 app.use((err,req,res,next)=>{
-    res.send("something went wrong");
+    let{statusCode=500,message="Something went wrong"}=err;
+    // res.status(statusCode) .send(message);
+    res.status(statusCode).render("error.ejs",{message});
 });
 app.listen(8080,()=>{
     console.log("server is listening to port 8080");
