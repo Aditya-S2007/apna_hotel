0b75df83a5257a6f084851034e7f3baf61ac30e4
Aditya Sharma <nomnomcat055@gmail.com>
2026-05-03 00:49:29 +0530

    login-page

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,17 +1,35 @@
 const express = require("express");
 const router = express.Router();  //{mergeParams:true}
+const User= require("../models/user.js");
+const wrapAsync = require("../utils/wrapAsync.js");
+const passport = require("passport");
+
 
 router.get("/signup",(req,res)=>{
     res.render("users/signup");
 });
 
-router.post("/signup", async(req,res)=>{
-    let{username,email,password} = req.body;
-    const newUser= new User ({email,username});
-    const registeredUser= await User.register(newUser,password);
+router.post("/signup",wrapAsync(async(req,res)=>{
+    try {
+        let{username,email,password} = req.body; // to exract data from form in signup.ejs
+    const newUser= new User ({email,username}); // creating new document in user collection
+    const registeredUser= await User.register(newUser,password); // insert schema with user password
     console.log(registeredUser);
-    req.flash("success","Welcome to Wanderlust");
+    req.flash("success","Welcome to Wanderlust"); // to flash msg of success
     res.redirect("/listings");
+    } catch (e) {
+       req.flash("error",e.message); // to convert error of flash msg
+       res.redirect("/signup"); 
+    }
+}));
+
+router.get("/login",(req,res)=>{
+    res.render("users/login.ejs");
+});
+
+router.post("/login",passport.authenticate("local",{failureRedirect:"/login",failureFlash:true}),async (req,res)=>{ // authi will help with built in authentiation then our stratgy is "local" , f.r "if we fail it will redirect to login page" , f.f "it will flash the error"
+   req.flash("success","Welcome back master.... its ur Wanderlust 😘");
+   res.redirect("/listings");
 });
 
 
