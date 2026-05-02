e8433218b2971d463a06bd2ff130b44de3343b61
Aditya Sharma <nomnomcat055@gmail.com>
2026-05-02 17:56:30 +0530

    sign-up form

--- backend/app.js
+++ backend/app.js
@@ -10,12 +10,16 @@ const ExpressError= require("./utils/ExpressError.js");
 // const { listingSchema, reviewSchema } = require("./schema.js");
 // const Review = require("./models/review.js");
 
-const listings= require("./routes/listing.js");
-const reviews= require("./routes/review.js");
+const listingRouter= require("./routes/listing.js");
+const reviewRouter= require("./routes/review.js");
+const userRouter= require("./routes/user.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 const session= require("express-session");
 const flash= require("connect-flash");
+const passport= require("passport");
+const LocalStrategy = require("passport-local");
+const User= require("./models/user.js");
 
 main().then(()=>{console.log("connected to db")})
     .catch((err)=>{
@@ -55,6 +59,11 @@ app.get("/",(req,res)=>{
 
 app.use(session(sessionOptions));
 app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use((req,res,next)=>{
     res.locals.success=req.flash("success");
@@ -62,8 +71,19 @@ app.use((req,res,next)=>{
     next();
 });
 
-app.use("/listings",listings);
-app.use("/listings/:id/reviews",reviews);
+// app.get("/demouser",async(req,res)=>{
+//     let fakeuser= new User({
+//         email:"nomnom@gmail.com",
+//         username:"alpha nomnom",
+//     });
+//     let registeredUser = await User.register(fakeuser,"nomnom"); // this will help user data to save with password
+//     res.send(registeredUser);
+
+// });
+
+app.use("/listings",listingRouter);
+app.use("/listings/:id/reviews",reviewRouter);
+app.use("/",userRouter);
 
 app.all(/(.*)/,(req,res,next)=>{
     next(new ExpressError(505,"page not found..!"));
--- backend/models/user.js
+++ backend/models/user.js
@@ -1,7 +1,7 @@
 const { required } = require("joi");
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const passportLocalMongoose= require("passport-local-mongoose");
+const passportLocalMongoose= require("passport-local-mongoose").default;
 
 const userSchema= new Schema({
     email:{
@@ -10,5 +10,5 @@ const userSchema= new Schema({
     },
 });
 
-User.plugin(passportLocalMongoose);
+userSchema.plugin(passportLocalMongoose);
 module.exports=mongoose.model("User",userSchema);
