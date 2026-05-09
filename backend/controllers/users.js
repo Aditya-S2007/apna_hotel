0b6c681850bec7e45f6f075cc4ab15f5b6c90cf9
Aditya Sharma <nomnomcat055@gmail.com>
2026-05-09 17:58:16 +0530

    controllers in users

---
+++ backend/controllers/users.js
@@ -0,0 +1,44 @@
+const User = require("../models/user");
+
+module.exports.renderSignupForm=(req,res)=>{
+    res.render("users/signup.ejs");
+};
+
+module.exports.signup=async(req,res)=>{
+    try {
+        let{username,email,password} = req.body; // to exract data from form in signup.ejs
+    const newUser= new User ({email,username}); // creating new document in user collection
+    const registeredUser= await User.register(newUser,password); // insert schema with user password
+    console.log(registeredUser);
+   req.login(registeredUser,(err)=>{
+            if(err){
+                return next(err);
+            }
+            req.flash("success","Welcome to Airbnb"); // to flash msg of success
+            res.redirect("/listings");
+        });
+    } catch (e) {
+       req.flash("error",e.message); // to convert error of flash msg
+       res.redirect("/signup"); 
+    }
+};
+
+module.exports.renderLoginForm=(req,res)=>{
+    res.render("users/login.ejs");
+};
+
+module.exports.login=async (req,res)=>{ // authi will help with built in authentiation then our stratgy is "local" , f.r "if we fail it will redirect to login page" , f.f "it will flash the error"
+   req.flash("success","Welcome back master.... its ur Wanderlust 😘");
+   let redirectUrl=res.locals.redirectUrl || "/listings"
+   res.redirect(redirectUrl);
+};
+
+module.exports.logout=(req,res,next)=>{
+    req.logout((err)=>{
+        if(err){
+            return next(err);
+        }
+        req.flash("success","logged you out!");
+        res.redirect("/listings");
+    })
+};
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -5,50 +5,19 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const passport = require("passport");
 const { saveredirectUrl } = require("../middleware.js");
 
+const userController = require("../controllers/users.js");
 
-router.get("/signup",(req,res)=>{
-    res.render("users/signup");
-});
 
-router.post("/signup",wrapAsync(async(req,res)=>{
-    try {
-        let{username,email,password} = req.body; // to exract data from form in signup.ejs
-    const newUser= new User ({email,username}); // creating new document in user collection
-    const registeredUser= await User.register(newUser,password); // insert schema with user password
-    console.log(registeredUser);
-   req.login(registeredUser,(err)=>{
-            if(err){
-                return next(err);
-            }
-            req.flash("success","Welcome to Airbnb"); // to flash msg of success
-            res.redirect("/listings");
-        });
-    } catch (e) {
-       req.flash("error",e.message); // to convert error of flash msg
-       res.redirect("/signup"); 
-    }
-}));
+router.get("/signup",userController.renderSignupForm);
 
-router.get("/login",(req,res)=>{
-    res.render("users/login.ejs");
-});
+router.post("/signup",wrapAsync(userController.signup));
 
-router.post("/login",saveredirectUrl,passport.authenticate("local",{failureRedirect:"/login",failureFlash:true}),async (req,res)=>{ // authi will help with built in authentiation then our stratgy is "local" , f.r "if we fail it will redirect to login page" , f.f "it will flash the error"
-   req.flash("success","Welcome back master.... its ur Wanderlust 😘");
-   let redirectUrl=res.locals.redirectUrl || "/listings"
-   res.redirect(redirectUrl);
-});
+router.get("/login",userController.renderLoginForm);
+
+router.post("/login",saveredirectUrl,passport.authenticate("local",{failureRedirect:"/login",failureFlash:true}),userController.login);
 
 //logout
-router.get("/logout",(req,res,next)=>{
-    req.logout((err)=>{
-        if(err){
-            return next(err);
-        }
-        req.flash("success","logged you out!");
-        res.redirect("/listings");
-    })
-});
+router.get("/logout",userController.logout);
 
 module.exports=router;
 
